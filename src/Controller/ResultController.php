4f48467a765f3f88317787b907731ae589a20e69
Andreea Sabadis <andreea.sabadis@evozon.com>
2020-04-03 14:51:52 +0300

    Resolve all little things from the PR comments

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -14,18 +14,28 @@ use QuizApp\Service\QuizInstanceService;
 
 class ResultController extends AbstractController
 {
+    /**
+     * @var QuizInstanceService
+     */
     private $quizInstanceService;
+
     /**
      * @var SessionInterface
      */
     private $session;
+
     /**
      * @var QuestionInstanceService
      */
     private $questionInstanceService;
 
-    public function __construct(RendererInterface $renderer, QuizInstanceService $quizInstanceService,QuestionInstanceService $questionInstanceService, SessionInterface $session)
-    {
+    public function __construct
+    (
+        RendererInterface $renderer,
+        QuizInstanceService $quizInstanceService,
+        QuestionInstanceService $questionInstanceService,
+        SessionInterface $session
+    ) {
         parent::__construct($renderer);
         $this->quizInstanceService = $quizInstanceService;
         $this->questionInstanceService = $questionInstanceService;
@@ -39,14 +49,15 @@ class ResultController extends AbstractController
         return $this->renderer->renderView('admin-results-listing.phtml', ['data' => $data]);
     }
 
-    public function getResult(Request $request, array $requestAttributes):Response
+    public function getResult(Request $request, array $requestAttributes): Response
     {
         $quizInstanceId = $requestAttributes['quiz_instance_id'];
         $quizInstance = $this->quizInstanceService->findQuiz($quizInstanceId);
         $questionsAnswers = $this->questionInstanceService->getAllQuestionsForQuizInstance($quizInstanceId);
 
         return $this->renderer->renderView
-        ('admin-results.phtml',
+        (
+            'admin-results.phtml',
             [
                 'quizInstance' => $quizInstance,
                 'questionsAnswers' => $questionsAnswers,
