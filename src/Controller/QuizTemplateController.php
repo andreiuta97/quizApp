ceff187432a740a6b587040f1eda9cf7f0aae213
Andreea Sabadis <andreea.sabadis@evozon.com>
2020-04-08 20:20:34 +0300

    Sort quizzes by name

--- src/Controller/QuizTemplateController.php
+++ src/Controller/QuizTemplateController.php
@@ -36,6 +36,7 @@ class QuizTemplateController extends AbstractController
      * @var QuestionTemplateService
      */
     private $questionTemplateService;
+
     /**
      * @var int
      */
@@ -56,6 +57,13 @@ class QuizTemplateController extends AbstractController
         $this->resultsPerPage = $resultsPerPage;
     }
 
+    /**
+     * Adds a quiz to database and redirects to "Quizzes Listing" page.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function addQuiz(Request $request, array $requestAttributes): Response
     {
         $info = $request->getParameters();
@@ -77,6 +85,13 @@ class QuizTemplateController extends AbstractController
         return new Response($body, '1.1', 200, '');
     }
 
+    /**
+     * Updates the selected quiz from the database and redirects to "Quizzes Listing" page.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function updateQuiz(Request $request, array $requestAttributes): Response
     {
         $id = $requestAttributes['id'];
@@ -89,6 +104,13 @@ class QuizTemplateController extends AbstractController
         return $response;
     }
 
+    /**
+     * Deletes the selected quiz from the database and redirects to "Quizzes Listing" page.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function deleteQuiz(Request $request, array $requestAttributes): Response
     {
         $id = $requestAttributes['id'];
@@ -100,6 +122,13 @@ class QuizTemplateController extends AbstractController
         return $response;
     }
 
+    /**
+     * Displays all quizzes from database in a paginated, filtered and sorted manner.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function getQuizzes(Request $request, array $requestAttributes): Response
     {
         $currentPage = $requestAttributes['page'] ?? 1;
@@ -108,9 +137,17 @@ class QuizTemplateController extends AbstractController
         $paginator = new Paginator($quizzesSearchResult->getCount(), $currentPage, $this->resultsPerPage);
 
         return $this->renderer->renderView('admin-quizzes-listing.phtml',
-            ['quizzes' => $quizzesSearchResult->getItems(), 'paginator' => $paginator]);
+            ['quizzes' => $quizzesSearchResult->getItems(), 'paginator' => $paginator, 'order' => $requestAttributes['order']]);
     }
 
+    /**
+     * Displays the "Add Quiz" page.
+     * All available questions will be displayed in the form.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function addNewQuiz(Request $request, array $requestAttributes): Response
     {
         $questionRepo = $this->repositoryManager->getRepository(QuestionTemplate::class);
@@ -120,12 +157,18 @@ class QuizTemplateController extends AbstractController
         return $this->renderer->renderView('admin-quiz-add.phtml', ['questions' => $questions]);
     }
 
+    /**
+     * Displays the "Edit Quiz page containing the form pre-filled with the selected quiz's information.
+     * All available questions will be displayed in the form.
+     *
+     * @param Request $request
+     * @param array $requestAttributes
+     * @return Response
+     */
     public function editQuiz(Request $request, array $requestAttributes): Response
     {
         $id = $requestAttributes['id'];
-        /**
-         * @var $quiz QuizTemplate
-         */
+        /** @var $quiz QuizTemplate */
         $quiz = $this->quizTemplateService->getQuiz($id);
         $questionRepo = $this->repositoryManager->getRepository(QuestionTemplate::class);
         $criteria = new Criteria();
